642a4c3ac7cfadb19c219f29a040db68a893b372
Masadavide <davide.masadm@gmail.com>
2022-01-13 15:24:18 +0100

    finito

---
+++ myscript.js
@@ -0,0 +1,9 @@
+let nomeEimmagine = new Vue(
+    {
+        el: '#container',
+        data:{
+            messaggio: 'ciao',
+            immagineUrl: 'https://www.venetoformazione.it/wp-content/uploads/2020/11/ottimizzare-immagini-display-retina.jpg'
+        }
+    }
+)
